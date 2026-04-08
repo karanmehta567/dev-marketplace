f5c7a252591ac4ea12e082a568abb553e567b379
karanmehta567 <karanmehta29.km@gmail.com>
2026-04-08 16:37:49 +0530

    feat: initial commit

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactCompiler: true,
+  turbopack: {
+    root: "D:/marketplace/my-app",
+  },
+};
+
+export default nextConfig;
